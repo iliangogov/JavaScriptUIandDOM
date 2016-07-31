1b9fe1d2c9a7016fdbe80f1ddf26098af253a735
iliangogov <iliangogov@gmail.com>
2016-07-31 23:59:31 +0300

    bouncingBall/Canvas.js more balls added,bang each other asswell

--- bouncingBall/Canvas.js
+++ bouncingBall/Canvas.js
@@ -1,25 +1,26 @@
-function init() {
     var myCanvas = document.querySelector('#myCanvas');
     var context = myCanvas.getContext('2d');
     var width=myCanvas.width;
     var height=myCanvas.height;
     var x = 100;
     var y = 200;
-    var dx = 4;
-    var dy = 4;
-    var radius = 20;
+    var dx = 3;
+    var dy = 3;
+    var radius = 40;
     var image;
     image = new Image();
     image.src = 'images/ball.png';
     var ball ={
         'x':x,
         'y':y,
+        'dx':dx,
+        'dy':dy,
+        'r':radius,
         'img':image
     };
     var balls=[];
     balls.push(ball);
 
-    /*var myCanvas = document.querySelector('#myCanvas');
     myCanvas.addEventListener('click', function (ev) {
         var newBallX = ev.clientX;
         var newBallY = ev.clientY;
@@ -28,34 +29,61 @@ function init() {
         var newBall = {
             'x': newBallX,
             'y': newBallY,
+            'dx':dx,
+            'dy':dy,
+            'r':radius,
             'img': img
         };
         balls.push(newBall);
-        draw(newBall);
         console.log(balls.length);
-    });*/
 
-   // window.setInterval('gameFlow()',20);
-    window.requestAnimationFrame(gameFlow);
-    function gameFlow() {
-        draw(ball);
+    });
 
-        function draw(ball) {
-            context.clearRect(0, 0,width,height);
-            context.beginPath();
-            context.drawImage(ball.img, ball.x - radius, ball.y - radius, radius * 2, radius * 2);
-           // context.closePath();
-            /*context.fillStyle="#0000ff";
-             context.arc(x,y,radius,0,Math.PI*2,true);
-             context.fill();*/
-            // Boundary Logic
-            if (ball.x < radius || ball.x > width - radius) dx = -dx;
-            if (ball.y < radius || ball.y > height - radius) dy = -dy;
-            ball.x += dx;
-            ball.y += dy;
-            window.requestAnimationFrame(gameFlow);
-        }
+function run() {
+    context.clearRect(0, 0, width, height);
+    for(b of balls) {
+        draw(b);
+        move(b);
     }
-};
+        window.requestAnimationFrame(run);
+
+}
+    function draw(ball) {
+        context.beginPath();
+        context.drawImage(ball.img, ball.x - radius, ball.y - radius, radius*2, radius*2);
+        context.closePath();
+    }
+    function move(ball) {
+        if (ball.x <= radius || ball.x >= width - radius) ball.dx = -ball.dx;
+        if (ball.y <= radius || ball.y >= height - radius) ball.dy = -ball.dy;
+        for( var other of balls){
+            if(isBallCollidingWith(ball,other)){
+                ball.dx = -ball.dx;
+                ball.dy = -ball.dy;
+            }
+        }
+        ball.x += ball.dx;
+        ball.y += ball.dy;
+    }
+    function isBallCollidingWith(ball,other){
+        if(other === ball){
+            return false;
+        }
+        var b1 = {
+                x: ball.x,
+                y: ball.y,
+                r: ball.r
+            },
+            b2 = {
+                x: other.x,
+                y: other.y,
+                r: other.r
+            };
+        var d = Math.sqrt((b1.x - b2.x) * (b1.x - b2.x) +
+            (b1.y - b2.y) * (b1.y - b2.y));
+        return d <= (b1.r + b2.r);
+    }
+
+
 
 
